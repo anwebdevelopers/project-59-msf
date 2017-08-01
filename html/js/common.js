d9c7704fb66ac113bb7b02ea4983bef57de73280
anwebdevelopers <anwebdev78@gmail.com>
2017-08-01 18:50:53 +0300

    01.08.2017

--- html/js/common.js
+++ html/js/common.js
@@ -18,6 +18,51 @@ $(function() {
     }
 
     /*******************************************************/
+    //popup
+    /*******************************************************/
+    $('.popup-with-move-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    });
+
+
+    //*****************************************************
+    //GOOGLE MAP
+    //*****************************************************
+    if (typeof google === 'object' && typeof google.maps === 'object' && $('#map').length) {
+        var markerPosition = new google.maps.LatLng(55.818602, 37.439970);
+
+        function initMap() {
+            var loc, map;
+
+            loc = new google.maps.LatLng(55.818602, 37.439970);
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 15,
+                center: loc,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                scrollwheel: false
+            });
+
+            var marker = new google.maps.Marker({
+                map: map,
+                position: markerPosition,
+                visible: true,
+                icon: 'img/icon-map.png'
+            });
+        }
+        initMap();
+        google.maps.event.addDomListener(window, 'load', initMap);
+    }
+
+    /*******************************************************/
     //Chrome Smooth Scroll
     /*******************************************************/
     try {
